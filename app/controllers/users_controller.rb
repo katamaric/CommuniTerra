4467690e2339b05e5150877d56fe751e5978a5bf
katamaric <katherinehcao@gmail.com>
2023-06-13 11:06:09 +0200

    commented out validations for sign up and added sign out in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      if params[:id] == "sign_out"
+        @user = current_user
+      else
+        @user = User.find(params[:id])
+      end
     end
 
     # Only allow a list of trusted parameters through.
