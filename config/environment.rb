bd633dca5c2f969c8ad3f136a8d668e218bed4f8
cboris <b.crochet@gmail.com>
2023-06-14 19:52:47 +0200

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'SSL0.OVH.NET',
+  :port => 587,
+  :user_name => ENV['APP_KEY'],
+  :password => ENV['APP_SECRET'],
+  :authentication => :login,
+  :enable_starttls_auto => true
+}
