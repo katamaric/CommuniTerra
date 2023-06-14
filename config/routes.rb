0be1ebcaa21d9db65d27087a63be1b31b1027f92
Hugalito <hugo.rischmann@me.com>
2023-06-14 16:15:47 +0200

    team page

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   resources :owned_plants
   resources :plants
   get 'static_pages/home'
+  get '/team', to: 'static_pages#team'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
