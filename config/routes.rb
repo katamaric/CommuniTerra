aa9b891e4bd1a6a5b4ec13f73676a9ef0abc04f0
katamaric <katherinehcao@gmail.com>
2023-06-14 11:50:08 +0200

    Profile info all listed in dashboard index and Edit profile now allows additional info to be updated as well

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   resources :listings
   resources :allotment_users
   resources :allotments
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
