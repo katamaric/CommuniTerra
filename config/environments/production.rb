6a95924b4492167691128432c9609efba604b504
cboris <b.crochet@gmail.com>
2023-06-16 17:19:12 +0200

    add reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "communiterra_thp_production"
 
+  config.action_mailer.default_url_options = { :host => 'communiterra.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
