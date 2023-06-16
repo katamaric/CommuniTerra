ce9add79f9cc6ca9431e7e5afa4892e09f839126
Tommy <sasida@hotmail.fr>
2023-06-16 15:15:26 +0200

    adding username sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,6 +59,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  
-  
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
 end
