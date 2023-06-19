1bd9e07dba99e57515a4ee5cd84a4837cbd2f052
katamaric <katherinehcao@gmail.com>
2023-06-19 11:20:56 +0200

    translation fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_114937) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_234704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,6 +136,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_114937) do
     t.string "phone_number"
     t.datetime "birthdate"
     t.text "bio"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -143,7 +144,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_114937) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
