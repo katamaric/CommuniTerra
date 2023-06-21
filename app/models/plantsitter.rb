7ebc303d32591106d02bf226c5b8ec76eed81771
Hugalito <hugo.rischmann@me.com>
2023-06-21 15:20:21 +0200

    deleted bad models again

--- app/models/plantsitter.rb
+++
@@ -1,4 +0,0 @@
-class PlantSitter < User
-  has_many :kept_plants_as_sitter, foreign_key: :sitter_id, class_name: 'KeptPlant'
-  has_many :plant_sittings_as_sitter, through: :kept_plants_as_sitter, source: :plant_sitting
-end
