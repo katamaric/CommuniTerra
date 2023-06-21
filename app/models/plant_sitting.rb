5b6d40d2baa77338a1abd5011324806100b62e71
Hugalito <hugo.rischmann@me.com>
2023-06-21 17:32:20 +0200

    cannot sit your own plants

--- app/models/plant_sitting.rb
+++ app/models/plant_sitting.rb
@@ -2,4 +2,14 @@ class PlantSitting < ApplicationRecord
   belongs_to :sitter, class_name: 'User', optional: true
   belongs_to :asker, class_name: 'User'
   has_many :kept_plants
+
+  validate :cannot_sit_own_plants
+
+  private
+
+  def cannot_sit_own_plants
+    if sitter_id == asker_id
+      errors.add(:sitter_id, "Vous ne pouvez pas garder vos propres plantes !")
+    end
+  end
 end
