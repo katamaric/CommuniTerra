c3178df34f7a68f7f7fb891d40cb1251c33b49ca
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-06-22 17:08:58 +0200

    listing validation fix

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ApplicationRecord
   validates :listing_title, presence: true, length: { maximum: 255 }
   validates :item_name, presence: true, length: { maximum: 255 }
   validates :description, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 999999 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :delivery_id, presence: true, inclusion: { in: Delivery.pluck(:id) }
 end
