f5c3c931df5b1d8c6dc2ea3af7d11bf2b4ca48d9
Tommy <sasida@hotmail.fr>
2023-06-22 13:34:01 +0200

    translation

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module CommuniterraThp
     # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join("extras")
     config.public_file_server.enabled = true
+
+    config.i18n.default_locale = :fr
   end
 end
