4356629530a52b3fc8fc3e9a535772702c63976e
Tommy <sasida@hotmail.fr>
2023-06-22 12:59:04 +0200

    validates

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :delivery
-  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
+  validates :listing_title, presence: true, length: { maximum: 255 }
+  validates :item_name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :delivery_id, presence: true, inclusion: { in: Delivery.pluck(:id) }
 end
