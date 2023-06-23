d343b5c63faa04ca81f04600194003dabb6397d4
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-06-23 19:33:13 +0200

    static controllers

---
+++ app/controllers/static_pages.controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController	
+  def home	
+  end	
+
+  def team	
+  end	
+
+  def contact	
+  end	
+
+  def faq	
+  end	
+
+  def historic	
+  end	
+
+  def mentions	
+  end	
+end	
