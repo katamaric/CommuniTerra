cd75e85c4dfeecedd00d0daa264709b7a15dbd10
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-06-23 13:30:00 +0200

    fix cart link
    Co-authored-by: katamaric <katamaric@users.noreply.github.com>

--- app/controllers/cart_listings_controller.rb
+++ app/controllers/cart_listings_controller.rb
@@ -14,17 +14,17 @@ class CartListingsController < ApplicationController
   
     if new_quantity <= listing.remaining_quantity
       cart_listing.update(quantity: new_quantity)
-      redirect_to cart_path, notice: 'Quantity updated successfully.'
+      redirect_to cart_path, notice: 'La quantité a bien été mise à jour.'
     else
-      flash[:alert] = 'The requested quantity exceeds the available quantity for this listing.'
+      flash[:alert] = 'La quantité ne peut pas être modifiée.'
       redirect_to cart_path
     end
   end  
 
   def destroy
-    @cart_listing = Cartlisting.find(params[:id])
+    @cart_listing = CartListing.find(params[:id])
     @cart_listing.destroy
-    redirect_to cart_path, notice: "listing removed from cart."
+    redirect_to cart_path, notice: "L'article a bien été enelevé de votre panier."
   end
 
 end
